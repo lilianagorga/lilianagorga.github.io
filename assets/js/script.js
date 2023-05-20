271bd086ea119639f80001f6e091aa0a0b360c6e
Liliana Gorga <gorgaliliana87@gmail.com>
2023-05-20 21:10:52 +0200

    styled header

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const logo = document.querySelector('.logo');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    logo.style.visibility = 'hidden';
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    showMenu = true;
+  } else {
+    setTimeout(function(){
+      logo.style.visibility = 'visible';
+    }, 500);
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+  }
+}
